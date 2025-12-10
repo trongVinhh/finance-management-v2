7ecfba07b9d144422b0d35bd9fa0a5fd25bdf9fe
Trong Vinh Lam <100221336+trongVinhh@users.noreply.github.com>
2025-12-10 17:14:54 +0700

    Increase pagination page size from 5 to 100

--- src/pages/Loans.tsx
+++ src/pages/Loans.tsx
@@ -148,7 +148,7 @@ export default function Loans() {
         columns={columns}
         dataSource={loans}
         rowKey="id"
-        pagination={{ pageSize: 5 }}
+        pagination={{ pageSize: 100 }}
         scroll={{ x: true }}
       />
 
